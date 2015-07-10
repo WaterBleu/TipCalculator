a7da920e8c159b425add296bc2e50ac30d707c94
WaterBleu <jeff7746@hotmail.com>
2015-07-10 10:37:13 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Jeff Huang on 2015-07-10.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Jeff Huang on 2015-07-10.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
